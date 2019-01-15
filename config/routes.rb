a4f36937fbade3771b37f7ce9e35a9e3aa78cd27
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-15 17:16:51 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get '/', to: 'secrets#index', as: 'secrets'
+  get '/', to: 'secrets#index'
+  get '/login', to: 'secrets#login'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
