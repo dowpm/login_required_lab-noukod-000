402dbdbfc31252e9cc45cd71fa63a2d1b370f1a2
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-15 17:55:20 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    redirect_to new unless params[:name].nil? || params[:name].empty?
+    session[:name] = params[:name]
+    redirect_to index
+  end
+
+  private
+
+  def require_login
+    return head(:forbidden) unless session.include? :name
+  end
 end
