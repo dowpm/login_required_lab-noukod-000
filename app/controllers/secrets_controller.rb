4471e0ecb648f3f745b1eb9cd3129b10b69f8cbc
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-15 18:21:42 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,6 +1,7 @@
 class SecretsController < ApplicationController
   def show
-    redirect_to controller: 'sessions', action: 'new' if current_user
-    # binding.pry
+    redirect_to controller: 'sessions', action: 'new' unless current_user
+    binding.pry
+    
   end
 end
