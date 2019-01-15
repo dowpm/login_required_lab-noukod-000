449efa818863a4d65c1b750891e4b0c0d57534b1
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-15 18:41:58 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    redirect_to(action: new) if params[:name].nil? || params[:name].empty?
+    return redirect_to(action: new) if params[:name].nil? || params[:name].empty?
     session[:name] = params[:name]
     redirect_to action: index
   end
